661a17f6d5074a9d6ede9a46dd253dc47ea595fd
Jenifer Medina <jenifermedina0327@gmail.com>
2026-09-10 16:22:41 -0300

    Creacion y conexion a Base de Datos

---
+++ config/conexion.php
@@ -0,0 +1,37 @@
+<?php
+    class Conectar {
+
+    protected $dbh;
+
+         
+    public function Conexion(){ 
+
+        try{
+
+            $this->dbh = new PDO("mysql:host=localhost;dbname=ventas;charset=utf8",
+               "root",
+                "",
+                 [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                 ]
+            
+             );
+
+                echo "Conexión exitosa a la base de datos";
+                return $this->dbh;
+
+             }catch (PDOException $e){
+                echo "Error BD: ". $e->getMessage();
+                die();
+
+            }
+
+        }
+
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+    }
+    $conexion = new Conectar();
+    $conexion->Conexion();
+?>
